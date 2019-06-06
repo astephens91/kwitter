209b045bd3dc8a14be0c1f4fbf8f3633df876965
Patterson Day <pxday22@gmail.com>
2019-06-06 14:47:39 -0400

    styling login page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loginThenGoToUserProfile as login } from "../actions";
+import { Form, Grid } from "semantic-ui-react";
 import Spinner from "react-spinkit";
 
 class LoginForm extends Component {
@@ -19,30 +20,42 @@ class LoginForm extends Component {
     const { isLoading, err } = this.props;
     return (
       <React.Fragment>
-        <h1>Login</h1>
-        <form onSubmit={this.handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={isLoading}>
-            Login
+        <Grid textAlign="center" style={{ height: '100vh' }}>
+          <Form className="loginForm" size="large">
+            <Form.Field>
+              <h1 class="login">Login</h1>
+              <form onSubmit={this.handleLogin}>
+                <label htmlFor="username"></label>
+                <Form.Input
+                  type="text"
+                  name="username"
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username/E-Mail"
+                  autoFocus
+                  required
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="password"></label>
+                <Form.Input
+                  type="password"
+                  name="password"
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  required
+                  onChange={this.handleChange}
+                />
+                <button class="ui red button" type="submit" disabled={isLoading}>
+                  Login
           </button>
-          <a href = "/register">JoinTheParty</a>
-        </form>
-        {isLoading && <Spinner name="circle" color="blue" />}
-        {err && <p style={{ color: "red" }}>{err}</p>}
+                <a class="ui black button" href="/register">Register</a>
+              </form>
+              {isLoading && <Spinner name="circle" color="blue" />}
+              {err && <p style={{ color: "red" }}>{err}</p>}
+            </Form.Field>
+          </Form>
+        </Grid>
       </React.Fragment>
     );
   }
