e2a4519f178e99f51bc055a020f25ad820cf6e24
Patterson Day <pxday22@gmail.com>
2019-06-07 10:28:21 -0400

    basic styling on register page

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import { LoginForm, UserProfile, RegisterForm, Navbar } from ".";
+import { LoginForm, UserProfile, RegisterForm } from ".";
 import 'semantic-ui-css/semantic.min.css'
 
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() =><React.Fragment> <Navbar/><LoginForm /></React.Fragment>} />
+        <Route exact path="/" render={() =><LoginForm />} />
         <Route exact path="/profile" render={() => <UserProfile />} />
         <Route exact path="/register" render={() => <RegisterForm />} />
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { loginThenGoToUserProfile as login } from "../actions";
-import { Form, Grid } from "semantic-ui-react";
+import { Form, Grid, /*Header*/ } from "semantic-ui-react";
 import Spinner from "react-spinkit";
 
 class LoginForm extends Component {
@@ -20,41 +20,45 @@ class LoginForm extends Component {
     const { isLoading, err } = this.props;
     return (
       <React.Fragment>
-        <Grid textAlign="center" style={{ height: '100vh' }}>
-          <Form className="loginForm" size="large">
-            <Form.Field>
-              <h1 class="login">Login</h1>
-              <form onSubmit={this.handleLogin}>
-                <label htmlFor="username"></label>
-                <Form.Input
-                  type="text"
-                  name="username"
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="Username/E-Mail"
-                  autoFocus
-                  required
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="password"></label>
-                <Form.Input
-                  type="password"
-                  name="password"
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Password"
-                  required
-                  onChange={this.handleChange}
-                />
-                <button class="ui red button" type="submit" disabled={isLoading}>
-                  Login
+        <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle">
+          <Grid.Column style={{ maxWidth: 350 }}>
+            {/* <Header color="teal" textAlign="center"> */}
+              <Form className="loginForm" size="large">
+                <Form.Field>
+                  <h1 className="login">Login</h1>
+                  <form onSubmit={this.handleLogin}>
+                    <label htmlFor="username"></label>
+                    <Form.Input
+                      type="text"
+                      name="username"
+                      icon="user"
+                      iconPosition="left"
+                      placeholder="Username/E-Mail"
+                      autoFocus
+                      required
+                      onChange={this.handleChange}
+                    />
+                    <label htmlFor="password"></label>
+                    <Form.Input
+                      type="password"
+                      name="password"
+                      icon="lock"
+                      iconPosition="left"
+                      placeholder="Password"
+                      required
+                      onChange={this.handleChange}
+                    />
+                    <button class="ui red button" type="submit" disabled={isLoading}>
+                      Login
           </button>
-                <a class="ui black button" href="/register">Register</a>
-              </form>
-              {isLoading && <Spinner name="circle" color="blue" />}
-              {err && <p style={{ color: "red" }}>{err}</p>}
-            </Form.Field>
-          </Form>
+                    <a class="ui black button" href="/register">Register</a>
+                  </form>
+                  {isLoading && <Spinner name="circle" color="blue" />}
+                  {err && <p style={{ color: "red" }}>{err}</p>}
+                </Form.Field>
+              </Form>
+            {/* </Header> */}
+          </Grid.Column>
         </Grid>
       </React.Fragment>
     );
